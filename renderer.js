3ad6b6236f03b95184d4b7fff84bb78b4a3d335c
Eric Arnebäck <arnebackeric@gmail.com>
2016-06-23 14:05:45 +0200

    refactor renderer.

--- renderer.js
+++ renderer.js
@@ -18,13 +18,7 @@ var vec3 = require('gl-vec3');
 
 const CAPSULE_SEGMENTS = 40;
 
-function Renderer(gl) {
-
-    /*
-     We use this single shader to render the GUI.
-     */
-    this.shader = createShader(gl, shaders.vert, shaders.frag);
-
+function Mesh(gl) {
     /*
      These buffers contain all the geometry data.
      */
@@ -32,21 +26,9 @@ function Renderer(gl) {
     this.colorBufferObject = createBuffer(gl, [], gl.ARRAY_BUFFER, gl.DYNAMIC_DRAW);
     this.uvBufferObject = createBuffer(gl, [], gl.ARRAY_BUFFER, gl.DYNAMIC_DRAW);
     this.indexBufferObject = createBuffer(gl, [], gl.ELEMENT_ARRAY_BUFFER, gl.DYNAMIC_DRAW);
-
-
-
-    this.particleTexture = createTexture(gl, particleImage);
-
 }
 
-Renderer.prototype.update = function (canvasWidth, canvasHeight) {
-    this.canvasWidth = canvasWidth;
-    this.canvasHeight = canvasHeight;
-}
-
-
-Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, emitters) {
-
+Mesh.prototype.prepareForRender = function() {
     /*
      Setup geometry buffers.
      */
@@ -60,21 +42,131 @@ Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, e
     this.colorBufferIndex = 0;
     this.uvBufferIndex = 0;
 
+    console.log("prepare: ", this.indexBufferIndex);
+
+}
+
+Mesh.prototype.draw = function(gl) {
+ console.log("draw: ", this.indexBufferIndex);
+    gl.drawElements(gl.TRIANGLES, (this.indexBufferIndex), gl.UNSIGNED_SHORT, 0);
+}
+
+Mesh.prototype.updateBuffers = function(gl, shader) {
+
+    this.positionBufferObject.update(this.positionBuffer);
+    gl.enableVertexAttribArray(shader.attributes.aPosition.location);
+    gl.vertexAttribPointer(shader.attributes.aPosition.location, 2, gl.FLOAT, false, 0, 0);
+    this.positionBufferObject.unbind();
+
+
+    this.colorBufferObject.update(this.colorBuffer);
+    gl.enableVertexAttribArray(shader.attributes.aColor.location);
+    gl.vertexAttribPointer(shader.attributes.aColor.location, 4, gl.FLOAT, false, 0, 0);
+    this.colorBufferObject.unbind();
+
+    this.uvBufferObject.update(this.uvBuffer);
+    gl.enableVertexAttribArray(shader.attributes.aUv.location);
+    gl.vertexAttribPointer(shader.attributes.aUv.location, 2, gl.FLOAT, false, 0, 0);
+    this.uvBufferObject.unbind();
+
+    this.indexBufferObject.update(this.indexBuffer);
+
+}
+
+Mesh.prototype._addIndex = function (index) {
+    this.indexBuffer[this.indexBufferIndex++] = index;
+};
+
+Mesh.prototype.update = function(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+}
+
+
+
+Mesh.prototype._addPosition = function (position) {
+
+    var x = ((position[0] + 1) / 2.0) * this.canvasHeight + (this.canvasWidth - this.canvasHeight) / 2.0;
+    var y = (((position[1] + 1) / 2.0) * this.canvasHeight);
+
+    this.positionBuffer[this.positionBufferIndex++] = x
+    this.positionBuffer[this.positionBufferIndex++] = y;
+};
+
+Mesh.prototype._addColor = function (color) {
+    this.colorBuffer[this.colorBufferIndex++] = color[0];
+    this.colorBuffer[this.colorBufferIndex++] = color[1];
+    this.colorBuffer[this.colorBufferIndex++] = color[2];
+    this.colorBuffer[this.colorBufferIndex++] = color[3];
+};
+
+Mesh.prototype._addUv = function (uv) {
+    this.uvBuffer[this.uvBufferIndex++] = uv[0];
+    this.uvBuffer[this.uvBufferIndex++] = uv[1];
+};
+
+/*
+ Add vertex that only has one color, and does not use a texture.
+ */
+Mesh.prototype._coloredVertex = function (position, color) {
+    // at this uv-coordinate, the font atlas is entirely white.
+    var whiteUv = [2.0, 2.0];
+
+    this._addPosition(position);
+    this._addColor(color);
+    this._addUv(whiteUv);
+};
+
+Mesh.prototype._texturedVertex = function (position, color, uv) {
+    this._addPosition(position);
+    this._addColor(color);
+    this._addUv(uv);
+};
+
+
+
+function Renderer(gl) {
+
+    /*
+     We use this single shader to render the GUI.
+     */
+    this.shader = createShader(gl, shaders.vert, shaders.frag);
+
+    this.mesh = new Mesh(gl);
+
+    this.particleTexture = createTexture(gl, particleImage);
+
+}
+
+Renderer.prototype.update = function (canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+
+    this.mesh.update(canvasWidth, canvasHeight);
+}
+
+
+Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, emitters) {
+
+
 
     /*
      Create geometry.
      */
+    this.mesh.prepareForRender();
 
     for (var i = 0; i < collisionBodies.length; ++i) {
 
         var body = collisionBodies[i];
 
         this._capsule(
+            this.mesh,
                 [body.p0[0] / WORLD_SCALE, body.p0[1] / WORLD_SCALE],
                 [body.p1[0] / WORLD_SCALE, body.p1[1] / WORLD_SCALE], body.radius / WORLD_SCALE, body.color, CAPSULE_SEGMENTS);
     }
     if(newCapsule != null) {
         this._capsule(
+            this.mesh,
             [newCapsule.p0[0] / WORLD_SCALE, newCapsule.p0[1] / WORLD_SCALE],
             [newCapsule.p1[0] / WORLD_SCALE, newCapsule.p1[1] / WORLD_SCALE], newCapsule.radius / WORLD_SCALE, newCapsule.color, CAPSULE_SEGMENTS);
     }
@@ -87,7 +179,7 @@ Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, e
         var p = emitter.position;
         var r = emitter.radius;
         
-        this._circle(p, r, [1.0, 0.0, 0.0], 20);
+        this._circle(this.mesh, p, r, [1.0, 0.0, 0.0], 20);
     }
 
 
@@ -99,8 +191,9 @@ Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, e
         var p = [particle.position[0] / WORLD_SCALE, particle.position[1] / WORLD_SCALE];
         var r =  renderMult*(particle.radius / WORLD_SCALE);
 
-        this._texturedBox([ p[0] - r, p[1] - r  ], [2*r, 2*r], particle.color);
+        this._texturedBox(this.mesh, [ p[0] - r, p[1] - r  ], [2*r, 2*r], particle.color);
     }
+
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -115,23 +208,7 @@ Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, e
         VAO_ext.bindVertexArrayOES(null);
 
 
-    this.positionBufferObject.update(this.positionBuffer);
-    gl.enableVertexAttribArray(this.shader.attributes.aPosition.location);
-    gl.vertexAttribPointer(this.shader.attributes.aPosition.location, 2, gl.FLOAT, false, 0, 0);
-    this.positionBufferObject.unbind();
-
-
-    this.colorBufferObject.update(this.colorBuffer);
-    gl.enableVertexAttribArray(this.shader.attributes.aColor.location);
-    gl.vertexAttribPointer(this.shader.attributes.aColor.location, 4, gl.FLOAT, false, 0, 0);
-    this.colorBufferObject.unbind();
-
-    this.uvBufferObject.update(this.uvBuffer);
-    gl.enableVertexAttribArray(this.shader.attributes.aUv.location);
-    gl.vertexAttribPointer(this.shader.attributes.aUv.location, 2, gl.FLOAT, false, 0, 0);
-    this.uvBufferObject.unbind();
-
-    this.indexBufferObject.update(this.indexBuffer);
+    this.mesh.updateBuffers(gl, this.shader);
 
 
     /*
@@ -155,53 +232,10 @@ Renderer.prototype.draw = function (gl, collisionBodies, particles,newCapsule, e
     gl.enable(gl.BLEND)
     gl.blendFunc(gl.ONE, gl.ONE)
 
-    gl.drawElements(gl.TRIANGLES, (this.indexBufferIndex), gl.UNSIGNED_SHORT, 0);
-
+    this.mesh.draw(gl);
 
 };
 
-Renderer.prototype._addIndex = function (index) {
-    this.indexBuffer[this.indexBufferIndex++] = index;
-};
-
-Renderer.prototype._addPosition = function (position) {
-
-    var x = ((position[0] + 1) / 2.0) * this.canvasHeight + (this.canvasWidth - this.canvasHeight) / 2.0;
-    var y = (((position[1] + 1) / 2.0) * this.canvasHeight);
-
-    this.positionBuffer[this.positionBufferIndex++] = x
-    this.positionBuffer[this.positionBufferIndex++] = y;
-};
-
-Renderer.prototype._addColor = function (color) {
-    this.colorBuffer[this.colorBufferIndex++] = color[0];
-    this.colorBuffer[this.colorBufferIndex++] = color[1];
-    this.colorBuffer[this.colorBufferIndex++] = color[2];
-    this.colorBuffer[this.colorBufferIndex++] = color[3];
-};
-
-Renderer.prototype._addUv = function (uv) {
-    this.uvBuffer[this.uvBufferIndex++] = uv[0];
-    this.uvBuffer[this.uvBufferIndex++] = uv[1];
-};
-
-/*
- Add vertex that only has one color, and does not use a texture.
- */
-Renderer.prototype._coloredVertex = function (position, color) {
-    // at this uv-coordinate, the font atlas is entirely white.
-    var whiteUv = [2.0, 2.0];
-
-    this._addPosition(position);
-    this._addColor(color);
-    this._addUv(whiteUv);
-};
-
-Renderer.prototype._texturedVertex = function (position, color, uv) {
-    this._addPosition(position);
-    this._addColor(color);
-    this._addUv(uv);
-};
 
 /*
  Render a box.
@@ -210,7 +244,7 @@ Renderer.prototype._texturedVertex = function (position, color, uv) {
  the optional `alpha` argument specifies the transparency of the box.
  default value of `alpha` is 1.0
  */
-Renderer.prototype._texturedBox = function (position, size, color) {
+Renderer.prototype._texturedBox = function (mesh, position, size, color) {
 
 
     if (typeof alpha === 'undefined') {
@@ -223,39 +257,39 @@ Renderer.prototype._texturedBox = function (position, size, color) {
     var tr = [position[0] + size[0], position[1]];
     var br = [position[0] + size[0], position[1] + size[1]];
 
-    var baseIndex = this.positionBufferIndex / 2;
+    var baseIndex = mesh.positionBufferIndex / 2;
 
     var c = [color[0], color[1], color[2], alpha];
 
     // vertex 1
     //this._coloredVertex(tl, c);
-    this._texturedVertex(tl, c, [0.0, 0.0] );
+    mesh._texturedVertex(tl, c, [0.0, 0.0] );
 
     // vertex 2
     //this._coloredVertex(bl, c);
-    this._texturedVertex(bl, c, [0.0, 1.0] );
+    mesh._texturedVertex(bl, c, [0.0, 1.0] );
 
     // vertex 3
     //  this._coloredVertex(tr, c);
-    this._texturedVertex(tr, c, [1.0, 0.0] );
+    mesh._texturedVertex(tr, c, [1.0, 0.0] );
 
 
     // vertex 4
 //    this._coloredVertex(br, c);
-    this._texturedVertex(br, c, [1.0, 1.0] );
+    mesh._texturedVertex(br, c, [1.0, 1.0] );
 
     // triangle 1
-    this._addIndex(baseIndex + 0);
-    this._addIndex(baseIndex + 1);
-    this._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 0);
+    mesh._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 2);
 
     // triangle 2
-    this._addIndex(baseIndex + 3);
-    this._addIndex(baseIndex + 2);
-    this._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 3);
+    mesh._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 1);
 };
 
-Renderer.prototype._box = function (position, size, color) {
+Renderer.prototype._box = function (mesh, position, size, color) {
 
 
     if (typeof alpha === 'undefined') {
@@ -268,32 +302,32 @@ Renderer.prototype._box = function (position, size, color) {
     var tr = [position[0] + size[0], position[1]];
     var br = [position[0] + size[0], position[1] + size[1]];
 
-    var baseIndex = this.positionBufferIndex / 2;
+    var baseIndex = mesh.positionBufferIndex / 2;
 
     var c = [color[0], color[1], color[2], alpha];
 
     // vertex 1
-    this._coloredVertex(tl, c);
+    mesh._coloredVertex(tl, c);
 
     // vertex 2
-    this._coloredVertex(bl, c);
+    mesh._coloredVertex(bl, c);
 
     // vertex 3
-    this._coloredVertex(tr, c);
+    mesh._coloredVertex(tr, c);
 
 
     // vertex 4
-    this._coloredVertex(br, c);
+    mesh._coloredVertex(br, c);
 
     // triangle 1
-    this._addIndex(baseIndex + 0);
-    this._addIndex(baseIndex + 1);
-    this._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 0);
+    mesh._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 2);
 
     // triangle 2
-    this._addIndex(baseIndex + 3);
-    this._addIndex(baseIndex + 2);
-    this._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 3);
+    mesh._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 1);
 };
 
 Renderer.prototype._unitCircle = function (position, theta, radius) {
@@ -303,7 +337,7 @@ Renderer.prototype._unitCircle = function (position, theta, radius) {
 /*
  A capsule is defined by a line segment between p1 and p2, and a radius.
  */
-Renderer.prototype._capsule = function (p1, p2, radius, color, segments) {
+Renderer.prototype._capsule = function (mesh, p1, p2, radius, color, segments) {
 
     // direction vector the line segment.
     var d = vec2.create();
@@ -312,8 +346,8 @@ Renderer.prototype._capsule = function (p1, p2, radius, color, segments) {
     var theta = Math.atan2(d[1], d[0]) - Math.PI / 2.0;
 
     // Draw the round parts at the end of the capsule.
-    this._arc(p1, radius, theta, color, segments);
-    this._arc(p2, radius, theta + Math.PI, color, segments);
+    this._arc(mesh, p1, radius, theta, color, segments);
+    this._arc(mesh, p2, radius, theta + Math.PI, color, segments);
 
 
     // normal to line segment.
@@ -322,48 +356,48 @@ Renderer.prototype._capsule = function (p1, p2, radius, color, segments) {
 
     var scratch = [0.0, 0.0];
 
-    var baseIndex = this.positionBufferIndex / 2;
+    var baseIndex = mesh.positionBufferIndex / 2;
 
 
     var alpha = 1.0;
     var c = [color[0], color[1], color[2], alpha];
 
     // vertex 1
-    this._coloredVertex(vec3.scaleAndAdd(scratch, p1, n, radius), c);
+    mesh._coloredVertex(vec3.scaleAndAdd(scratch, p1, n, radius), c);
 
     // vertex 2
-    this._coloredVertex(vec3.scaleAndAdd(scratch, p2, n, radius), c);
+    mesh._coloredVertex(vec3.scaleAndAdd(scratch, p2, n, radius), c);
 
     // vertex 3
-    this._coloredVertex(vec3.scaleAndAdd(scratch, p1, n, -radius), c);
+    mesh._coloredVertex(vec3.scaleAndAdd(scratch, p1, n, -radius), c);
 
     // vertex 4
-    this._coloredVertex(vec3.scaleAndAdd(scratch, p2, n, -radius), c);
+    mesh._coloredVertex(vec3.scaleAndAdd(scratch, p2, n, -radius), c);
 
 
     // triangle 1
-    this._addIndex(baseIndex + 2);
-    this._addIndex(baseIndex + 1);
-    this._addIndex(baseIndex + 0);
+    mesh._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 0);
 
 
     // triangle 2
-    this._addIndex(baseIndex + 1);
-    this._addIndex(baseIndex + 2);
-    this._addIndex(baseIndex + 3);
+    mesh._addIndex(baseIndex + 1);
+    mesh._addIndex(baseIndex + 2);
+    mesh._addIndex(baseIndex + 3);
 
 
 }
 
-Renderer.prototype._arc = function (centerPosition, radius, direction, color, segments) {
+Renderer.prototype._arc = function (mesh, centerPosition, radius, direction, color, segments) {
 
 
-    var baseIndex = this.positionBufferIndex / 2;
+    var baseIndex = mesh.positionBufferIndex / 2;
 
     var c = [color[0], color[1], color[2], 1.0];
 
     // add center vertex.
-    this._coloredVertex(centerPosition, c);
+    mesh._coloredVertex(centerPosition, c);
     var centerVertexIndex = baseIndex + 0;
 
     var stepSize = (2 * Math.PI) / segments;
@@ -375,16 +409,16 @@ Renderer.prototype._arc = function (centerPosition, radius, direction, color, se
             var p =
                 [centerPosition[0] + radius * Math.cos(theta + direction), centerPosition[1] + radius * Math.sin(theta + direction)];
 
-            this._coloredVertex(p, c);
+            mesh._coloredVertex(p, c);
         } else {
             var p =
                 [centerPosition[0] + radius * Math.cos(theta + direction), centerPosition[1] + radius * Math.sin(theta + direction)];
 
-            this._coloredVertex(p, c);
+            mesh._coloredVertex(p, c);
 
-            this._addIndex(curIndex + 0);
-            this._addIndex(curIndex - 1);
-            this._addIndex(centerVertexIndex);
+            mesh._addIndex(curIndex + 0);
+            mesh._addIndex(curIndex - 1);
+            mesh._addIndex(centerVertexIndex);
         }
     }
 };
@@ -394,15 +428,15 @@ Renderer.prototype._arc = function (centerPosition, radius, direction, color, se
  Render a circle, where the top-left corner of the circle is `position`
  Where `segments` is how many triangle segments the triangle is rendered with.
  */
-Renderer.prototype._circle = function (centerPosition, radius, color, segments) {
+Renderer.prototype._circle = function (mesh, centerPosition, radius, color, segments) {
 
 
-    var baseIndex = this.positionBufferIndex / 2;
+    var baseIndex = mesh.positionBufferIndex / 2;
 
     var c = [color[0], color[1], color[2], 1.0];
 
     // add center vertex.
-    this._coloredVertex(centerPosition, c);
+    mesh._coloredVertex(centerPosition, c);
     var centerVertexIndex = baseIndex + 0;
 
     var stepSize = (2 * Math.PI) / segments;
@@ -412,14 +446,14 @@ Renderer.prototype._circle = function (centerPosition, radius, color, segments) 
         // for first frame, we only create one vertex, and no triangles
         if (theta == 0) {
             var p = this._unitCircle(centerPosition, theta, radius);
-            this._coloredVertex(p, c);
+            mesh._coloredVertex(p, c);
         } else {
             var p = this._unitCircle(centerPosition, theta, radius);
-            this._coloredVertex(p, c);
+            mesh._coloredVertex(p, c);
 
-            this._addIndex(curIndex + 0);
-            this._addIndex(curIndex - 1);
-            this._addIndex(centerVertexIndex);
+            mesh._addIndex(curIndex + 0);
+            mesh._addIndex(curIndex - 1);
+            mesh._addIndex(centerVertexIndex);
         }
     }
 };
